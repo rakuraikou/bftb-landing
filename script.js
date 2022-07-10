e36ae4518edbe1cbbfc8a8de7ed93e23b09c7308
THUNDER <lenicarusdo@gmail.com>
2022-07-09 22:27:17 -0400

    yaayyy bootstrap
    updated a ton of stuff with bootstrap classes, built the base for responsive alt menu......agh

---
+++ script.js
@@ -0,0 +1,8 @@
+function mobileLinks() {
+    var x = document.getElementById("mobilelinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
